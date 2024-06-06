cca72b4fa81c875442a30774f1a5abb083cbc915
Aqillaprly <aqillaaprly.gmail.com>
2024-06-07 00:52:06 +0700

    first practicum

--- algoritma/week10/Queue04.java
+++ algoritma/week10/Queue04.java
@@ -1,56 +1,76 @@
-public class Queue04{
-int max, size, front, rear, q;
+public class Queue04 {
+    int max, size, front, rear;
+    int[] q;
 
-    public Queue04(int n){
+    public Queue04(int n) {
         max = n;
         Create();
     }
 
-    public void Create(){
+    public void Create() {
         q = new int[max];
         size = 0;
         front = rear = -1;
     }
 
-    public boolean isEmpty(){
-        if(size == 0){
-            return true;
-        }else {
-            return false;
-        }
+    public boolean isEmpty() {
+        return size == 0;
     }
 
-    public boolean isFull(){
-        if(!isEmpty()){
-            System.out.println("The first element : " + q[front]);
-        }else{
+    public boolean isFull() {
+        return size == max;
+    }
+
+    public void peek() {
+        if (!isEmpty()) {
+            System.out.println("The first element: " + q[front]);
+        } else {
             System.out.println("Queue is still empty");
         }
     }
 
-    public void peek(){
-        if(!isEmpty()){
-            System.out.println("The first element :" + q[front]);
-        }else{
-            System.out.println("Queue is still empty ");
+    public void enqueue(int value) {
+        if (isFull()) {
+            System.out.println("Queue is full");
+            return;
         }
+        if (isEmpty()) {
+            front = 0;
+        }
+        rear = (rear + 1) % max;
+        q[rear] = value;
+        size++;
     }
 
-    public void print(){
-        if(!isEmpty()){
+    public int dequeue() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+            return -1; 
+        }
+        int removedValue = q[front];
+        front = (front + 1) % max;
+        size--;
+        if (isEmpty()) {
+            front = rear = -1;
+        }
+        return removedValue;
+    }
+
+    public void print() {
+        if (isEmpty()) {
             System.out.println("Queue is still empty");
-        }else {
+        } else {
             int i = front;
             while (i != rear) {
-                System.out.println(q[i] +" ");
-                i = (i+1) %max;
+                System.out.print(q[i] + " ");
+                i = (i + 1) % max;
             }
-            System.out.println(q[i]+"");
-            System.out.println("Element amount :" +size);
+            System.out.print(q[i] + " ");
+            System.out.println("\nElement amount: " + size);
         }
-    }    
+    }
 
-    public void clear(){
-        
+    public void clear() {
+        Create();
     }
 }
