ea8209f00ce97f2c891c4155ee8eaa32a9e6a041
Aqillaprly <aqillaaprly.gmail.com>
2024-06-07 01:05:04 +0700

    practicum 2

---
+++ algoritma/week10/Passenger04.java
@@ -0,0 +1,12 @@
+public class Passenger04 {
+    String name, cityOrigin, cityDestination;
+    int ticketAmount, price;
+
+    public Passenger04(String nm, String co, String cd, int ta, int pr) {
+        name = nm;
+        cityOrigin = co;
+        cityDestination = cd;
+        ticketAmount = ta;
+        price = pr;
+    }
+}
---
+++ algoritma/week10/PassengerMain04.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class PassengerMain04 {
+    public static void menu(){
+        System.out.println("Choose menu: ");
+        System.out.println("1. Queue");
+        System.out.println("2. Dequeue");
+        System.out.println("3. Check first queue");
+        System.out.println("4. Check all queue");
+        System.out.println("=======================");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Insert maximum queue : ");
+        int max = sc.nextInt();
+
+        QueuePassenger04 queuePassenger = new QueuePassenger04(max);
+
+        int choose;
+        do {
+            menu();
+            choose = sc.nextInt();
+            switch (choose) {
+                case 1:
+                    sc.nextLine();
+                    System.out.print("Name: ");
+                    String nm = sc.nextLine();
+                    System.out.print("City Origin: ");
+                    String cOrg = sc.nextLine();
+                    System.out.print("City Destination: ");
+                    String cDes= sc.nextLine();
+                    System.out.print("Ticket Amount: ");
+                    int ticket = sc.nextInt();
+                    System.out.print("Price: ");
+                    int price = sc.nextInt();
+                    Passenger04 p = new Passenger04(nm, cOrg, cDes, ticket, price);
+                    sc.nextLine();
+                    queuePassenger.Enqueue(p);
+                    break;
+                case 2:
+                    Passenger04 data = queuePassenger.Dequeue();
+                    if (!"".equals(data.name) && !"".equals(data.cityOrigin) &&
+                            !"".equals(data.cityDestination) && !"".equals(data.ticketAmount)
+                            && !"".equals(data.price)) {
+                        System.out.println("Data removed : " + data.name + " " + data.cityOrigin
+                        + " " + data.cityDestination + " " + data.ticketAmount + " " + data.price);
+                        break;
+                    }
+                case 3:
+                    queuePassenger.peek();
+                    break;
+                case 4:
+                    queuePassenger.print();
+                    break;
+            }
+
+        } while (choose <= 4 && choose >=1);
+
+        sc.close();
+    }
+}
