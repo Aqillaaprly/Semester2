fa0a7baa7881611e4107151c370bf2675a19eb1e
Aqillaprly <aqillaaprly.gmail.com>
2024-06-25 22:39:33 +0700

    assigment 1

---
+++ algoritma/week12/assigment1/Node1.java
@@ -0,0 +1,11 @@
+public class Node1 {
+    int data;
+    Node1 next;
+    Node1 prev;
+
+    public Node1(int data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
