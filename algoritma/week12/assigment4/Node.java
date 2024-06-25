f6416dd88ad97fa91c2e67adb83f0a984a144fde
Aqillaprly <aqillaaprly.gmail.com>
2024-06-25 23:01:00 +0700

    assigment 4

---
+++ algoritma/week12/assigment4/Node.java
@@ -0,0 +1,11 @@
+public class Node {
+    Student data;
+    Node next;
+    Node prev;
+
+    public Node(Student data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
