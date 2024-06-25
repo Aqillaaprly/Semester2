6f6e229d6ca85f3b94fdfbfaa96f05be36de8e1c
Aqillaprly <aqillaaprly.gmail.com>
2024-06-25 22:52:43 +0700

    assigment 3

---
+++ algoritma/week12/assigment3/Node.java
@@ -0,0 +1,13 @@
+public class Node {
+    int queueNumber;
+    String name;
+    Node next;
+    Node prev;
+
+    public Node(int queueNumber, String name) {
+        this.queueNumber = queueNumber;
+        this.name = name;
+        this.next = null;
+        this.prev = null;
+    }
+}
