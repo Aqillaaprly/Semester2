997b7d47befc71d3ceb73ed728a5a8d5a8c80d2e
Aqillaprly <aqillaaprly.gmail.com>
2024-06-26 12:32:24 +0700

    practicum 1

---
+++ algoritma/week14/Node04.java
@@ -0,0 +1,16 @@
+import org.w3c.dom.Node;
+
+public class Node04{
+    int data;
+    Node04 left;
+    Node04 right;
+
+    public Node04(){
+    }
+
+    public Node04(int data) {
+        this.left = null;
+        this.data = data;
+        this.right = null;
+    }
+}
